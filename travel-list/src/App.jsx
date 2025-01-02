5372291439535c1846d48984512fff096bd39b31
Faidunni <hazeezfaizat@gmail.com>
2025-01-02 21:39:35 +0000

    percentage

--- travel-list/src/App.jsx
+++ travel-list/src/App.jsx
@@ -9,9 +9,15 @@ import { useState } from "react";
 function App() {
   const [items, setItem] = useState([]);
 
+  // calculation your stats, how to not do it
+  // const [numItems, setNumItems] = useState(0);
+
+  // how to actaully do it
+
   // add item
   function handleAddItem(item) {
     setItem((items) => [...items, item]);
+    // setNumItems((num) => num + 1);
   }
 
   // delete an item
@@ -37,7 +43,7 @@ function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItem={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -116,55 +122,18 @@ function Item({ item, onDeleteItem, onToggleItem }) {
     </li>
   );
 }
-function Stats() {
-  const [formData, setFormData] = useState([]);
 
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
+function Stats({ items }) {
+  // calculating the stats;
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
 
-  function handleAddDetails(data) {
-    setFormData((formData) => [...formData, data]);
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-
-    const newInfo = { name, email };
-    console.log(newInfo);
-    handleAddDetails(newInfo);
-    setEmail("");
-    setName("");
-  }
   return (
     <footer className="stats">
-      <em>You have X items on your list, and you already packed X(X%)</em>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <ul>
-        {formData.map((form, index) => (
-          <div key={index}>
-            <li>
-              <strong>Name:</strong>
-              {form.name}
-            </li>
-            <li>
-              <strong>Email:</strong>
-              {form.email}
-            </li>
-          </div>
-        ))}
-      </ul>
+      <em>
+        You have {numItems} items on your list, and you already packed{" "}
+        {numPacked} (X%)
+      </em>
     </footer>
   );
 }
