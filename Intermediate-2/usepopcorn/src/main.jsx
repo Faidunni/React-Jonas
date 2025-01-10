154910ee198736a8914a0601e1a2b8155928296f
Faidunni <hazeezfaizat@gmail.com>
2025-01-10 21:49:38 +0000

    Text-expander

--- Intermediate-2/usepopcorn/src/main.jsx
+++ Intermediate-2/usepopcorn/src/main.jsx
@@ -3,10 +3,27 @@ import { createRoot } from "react-dom/client";
 // import './index.css'
 // import App from './App.jsx'
 import StarRating from "./StarRating";
+import { useState } from "react";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App/> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
+    <Test />
   </StrictMode>
 );
